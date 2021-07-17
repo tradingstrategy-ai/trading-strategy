8ead5bf18ffc82d27f4cc700244da934441ba830
Mikko Ohtamaa <mikko@opensourcehacker.com>
2021-07-17 22:01:37 +0200

    Added code snippet example

--- capitalgram/utils/columnar.py
+++ capitalgram/utils/columnar.py
@@ -8,6 +8,20 @@ def iterate_columnar_dicts(inp: Dict[str, list]) -> Iterable[Dict[str, object]]:
 
     Useful for constructing rows/objects out from :py:class:`pyarrow.Table` or :py:class:`pyarrow.RecordBatch`.
 
+    Example:
+
+    .. code-block:: python
+
+        @classmethod
+        def create_from_pyarrow_table(cls, table: pa.Table) -> "PairUniverse":
+            pairs = {}
+            for batch in table.to_batches(max_chunksize=5000):
+                d = batch.to_pydict()
+                for row in iterate_columnar_dicts(d):
+                    pairs[row["pair_id"]] = DEXPair.from_dict(row)
+
+            return PairUniverse(pairs=pairs)
+
     :param inp: Input dictionary of lists e.g. one from :py:method:`pyarrow.RecordBatch.to_pydict`. All lists in the input must be equal length.
 
     :return: Iterable that gives one dictionary per row after transpose
