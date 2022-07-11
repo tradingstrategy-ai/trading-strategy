61fa044401acbb249e87fb99af55f043f0faef2c
Mikko Ohtamaa <mikko@opensourcehacker.com>
2022-07-11 13:21:42 +0900

    Add helpful exception message for damaged parquets

--- tradingstrategy/reader.py
+++ tradingstrategy/reader.py
@@ -5,10 +5,17 @@ import logging
 from pathlib import Path
 
 import pyarrow as pa
-from pyarrow import parquet as pq
-
+from pyarrow import parquet as pq, ArrowInvalid
 
 logger = logging.getLogger(__name__)
+
+
+class BrokenData(Exception):
+    """Raised when we cannot read Parquet file for some reason."""
+
+    def __init__(self, msg: str, path: Path):
+        super().__init__(msg)
+        self.path = path
 
 
 def read_parquet(path: Path) -> pa.Table:
@@ -22,5 +29,14 @@ def read_parquet(path: Path) -> pa.Table:
     f = path.as_posix()
     logger.info("Reading Parquet %s", f)
     # https://arrow.apache.org/docs/python/parquet.html
-    table = pq.read_table(f)
+    try:
+        table = pq.read_table(f)
+    except ArrowInvalid as e:
+        raise BrokenData(f"Could not read Parquet file: {f}\n"
+                         f"Probably a corrupted download.\n"
+                         f"See https://tradingstrategy.ai/docs/programming/troubleshooting.html#resetting-the-download-cache\n"
+                         f"for instructions how to clear download cache and remove corrupted files, or try the command line:\n"
+                         f"rm '{f}'",
+                         path=path) \
+                        from e
     return table
