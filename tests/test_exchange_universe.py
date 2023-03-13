949db25b98ad6b07f76b6d9a982b6fa7947a4121
Hieu Nguyen <hieu@tradingstrategy.ai>
2023-03-14 01:06:57 +0200

    Fix ExchangeUniverse filter

--- tests/test_exchange_universe.py
+++ tests/test_exchange_universe.py
@@ -12,3 +12,35 @@ def test_look_up_by_factory(persistent_test_client: Client):
     assert pancakewap_v2, "No pancakes :("
     assert pancakewap_v2.name == "PancakeSwap v2"
 
+
+def test_look_up_by_chain_and_name(persistent_test_client: Client):
+    """Look up a Uniswap v2 compatible exchange by its slug."""
+
+    client = persistent_test_client
+
+    exchange_universe = client.fetch_exchange_universe()
+    
+    quickswap = exchange_universe.get_by_chain_and_name(ChainId.polygon, "Quickswap")
+    assert quickswap
+    assert quickswap.exchange_slug == "quickswap"
+
+    # there should be only 1 quickswap on Polygon
+    nonexist = exchange_universe.get_by_chain_and_name(ChainId.ethereum, "Quickswap")
+    assert nonexist is None
+
+
+
+def test_look_up_by_chain_and_slug(persistent_test_client: Client):
+    """Look up a Uniswap v2 compatible exchange by its slug."""
+
+    client = persistent_test_client
+
+    exchange_universe = client.fetch_exchange_universe()
+    
+    quickswap = exchange_universe.get_by_chain_and_slug(ChainId.polygon, "quickswap")
+    assert quickswap
+    assert quickswap.name == "Quickswap"
+
+    # there should be only 1 quickswap on Polygon
+    nonexist = exchange_universe.get_by_chain_and_slug(ChainId.ethereum, "quickswap")
+    assert nonexist is None
