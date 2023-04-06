b8564d5cb413d632f6a101db312d6ddbfd47e640
alex urban <alexurban12@gmail.com>
2023-04-06 15:36:34 +0200

    refactor charting tests

--- tests/test_charting.py
+++ tests/test_charting.py
@@ -1,19 +1,20 @@
 """Charting functionality tests."""
 
 import pandas as pd
+import pytest
 from IPython.core.display_functions import display
 from pandas.core.groupby import DataFrameGroupBy
 
 from tradingstrategy.chain import ChainId
 from tradingstrategy.charting.candle_chart import visualise_ohlcv, make_candle_labels
 from tradingstrategy.client import Client
-from tradingstrategy.pair import PandasPairUniverse
+from tradingstrategy.pair import PandasPairUniverse, DEXPair
 from tradingstrategy.timebucket import TimeBucket
 
 
-def test_candle_chart(persistent_test_client: Client):
-    """Draw a candle chart."""
-
+@pytest.fixture(scope="module")
+def candles_and_pair(persistent_test_client: Client) -> tuple[pd.DataFrame, DEXPair]:
+    """Get candles and pair for testing."""
     client = persistent_test_client
     exchange_universe = client.fetch_exchange_universe()
     pairs_df = client.fetch_pair_universe().to_pandas()
@@ -35,7 +36,16 @@ def test_candle_chart(persistent_test_client: Client):
         TimeBucket.d1,
         progress_bar_description=f"Download data for {pair.get_ticker()}"
     )
+    
+    return candles, pair
 
+
+def test_candle_chart(candles_and_pair: tuple[pd.DataFrame, DEXPair]):
+    """Draw a candle chart."""
+
+    
+    candles, pair = candles_and_pair
+    
     figure = visualise_ohlcv(
         candles,
         chart_name=f"{pair.base_token_symbol} - {pair.quote_token_symbol} price chart",
@@ -48,30 +58,10 @@ def test_candle_chart(persistent_test_client: Client):
     # display(figure)
 
 
-def test_candle_labels(persistent_test_client: Client):
+def test_candle_labels(candles_and_pair: tuple[pd.DataFrame, DEXPair]):
     """Create labels for a candle chart."""
 
-    client = persistent_test_client
-    exchange_universe = client.fetch_exchange_universe()
-    pairs_df = client.fetch_pair_universe().to_pandas()
-
-    # Create filtered exchange and pair data
-    exchange = exchange_universe.get_by_chain_and_slug(ChainId.bsc, "pancakeswap-v2")
-    pair_universe = PandasPairUniverse.create_single_pair_universe(
-            pairs_df,
-            exchange,
-            "WBNB",
-            "BUSD",
-            pick_by_highest_vol=True,
-        )
-
-    pair = pair_universe.get_single()
-
-    candles: pd.DataFrame = client.fetch_candles_by_pair_ids(
-        {pair.pair_id},
-        TimeBucket.d1,
-        progress_bar_description=f"Download data for {pair.get_ticker()}"
-    )
+    candles, pair = candles_and_pair
 
     assert "label" not in candles.columns
 
