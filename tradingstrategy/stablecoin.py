57b1c6c8b7df0bea3f9e07d29650f338e5386252
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-05-19 19:52:56 +0200

    More stablecoins

--- tradingstrategy/stablecoin.py
+++ tradingstrategy/stablecoin.py
@@ -4,11 +4,12 @@
 #: Note that it is *not* safe to to check the token symbol to know if a token is a specific stablecoin,
 #: but you always need to check the contract address.
 #: Checking against this list only works
-STABLECOIN_LIKE = ("DAI", "USDC", "USDT", "DAI", "BUSD", "UST", "USDN")
+STABLECOIN_LIKE = ("DAI", "USDC", "USDT", "DAI", "BUSD", "UST", "USDN", "LUSD", "VUSD", "USDV", "EUROe", "EURT", "USDP", "iUSD", "USDS", "gmUSD", "USDR", "RAI", "EURS", "TUSD", "EURe", "USD+", "EUROC", "USDs")
 
 
 #: Stablecoins plus their interest wrapped counterparts on Compound and Aave.
-WRAPPED_STABLECOIN_LIKE = ("cUSDC", "cUSDT", "sUSD", "aDAI")
+#: Also contains other derivates.
+WRAPPED_STABLECOIN_LIKE = ("cUSDC", "cUSDT", "sUSD", "aDAI", "cDAI", "tfUSDC", "alUSD", "agEUR", "gmdUSDC", "gDAI")
 
 #: All stablecoin likes - both interested bearing and non interest bearing.
 ALL_STABLECOIN_LIKE = STABLECOIN_LIKE + WRAPPED_STABLECOIN_LIKE
@@ -31,4 +32,4 @@ def is_stablecoin_like(token_symbol: str, symbol_list=ALL_STABLECOIN_LIKE) -> bo
         Which filtering list we use.
     """
     assert isinstance(token_symbol, str), f"We got {token_symbol}"
-    return (token_symbol in symbol_list)
+    return (token_symbol.upper() in symbol_list)
