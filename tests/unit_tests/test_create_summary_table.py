972e75ebc73f985007431ed3862249f317efc827
alex urban <alexurban12@gmail.com>
2023-07-05 09:50:20 +0200

    add data to tests

--- tests/unit_tests/test_create_summary_table.py
+++ tests/unit_tests/test_create_summary_table.py
@@ -1,4 +1,5 @@
 import datetime
+import pandas as pd
 from tradingstrategy.utils.summarydataframe import (
     create_summary_table,
     as_dollar,
@@ -20,10 +21,17 @@ def test_create_summary_table_single_column():
 
     assert df.shape == (4, 1)
 
+    data = ["10.00%", "30.00%", "$320.00", "5 days 2 hours"]
+    index = ["Annualised return %", "Lifetime return %", "Realised PnL", "Trade period"]
+    manual_df = pd.DataFrame(data, index=index)
+    manual_df.index.name="Returns"
+    manual_df.columns = [""]
+    assert df.equals(manual_df)
+
 
 def test_create_summary_table_multiple_columns():
     
-    data3 = {
+    data = {
         "Number of positions": [
             as_integer(3),
             as_integer(5),
@@ -41,8 +49,14 @@ def test_create_summary_table_multiple_columns():
         ],
     }
 
-    df3 = create_summary_table(
-        data3, ["Winning", "Losing", "Total"], "Closed Positions"
+    df = create_summary_table(
+        data, ["Winning", "Losing", "Total"], "Closed Positions"
     )
 
-    assert df3.shape == (3, 3)
+    assert df.shape == (3, 3)
+
+    data = [["3", "5", "8"], ["37.50%", "62.50%", "100.00%"], ["6.00%", "-2.00%", "3.00%"]]
+    index = ["Number of positions", "% of total", "Average PnL %"]
+    manual_df = pd.DataFrame(data, index=index, columns=["Winning", "Losing", "Total"])
+    manual_df.index.name="Closed Positions"
+    assert df.equals(manual_df)
