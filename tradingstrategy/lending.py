b8db89b02f8a5aeafd687dbcacdd13677a7a8f7a
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-08-20 13:21:42 +0300

    Add code example

--- tradingstrategy/lending.py
+++ tradingstrategy/lending.py
@@ -342,6 +342,56 @@ class LendingCandleUniverse:
     easy to do credit supply, shorting and longing in the same trading strategy.
 
     See also :py:class:`LendingMetricUniverse`.
+
+    Example:
+
+    .. code-block:: python
+
+        universe = client.fetch_lending_reserve_universe()
+
+        usdt_desc = (ChainId.polygon, LendingProtocolType.aave_v3, "USDT")
+        usdc_desc = (ChainId.polygon, LendingProtocolType.aave_v3, "USDC")
+
+        limited_universe = universe.limit([usdt_desc, usdc_desc])
+
+        usdt_reserve = limited_universe.resolve_lending_reserve(usdt_desc)
+        usdc_reserve = limited_universe.resolve_lending_reserve(usdc_desc)
+
+        lending_candle_type_map = client.fetch_lending_candles_for_universe(
+            limited_universe,
+            TimeBucket.d1,
+            start_time=pd.Timestamp("2023-01-01"),
+            end_time=pd.Timestamp("2023-02-01"),
+        )
+
+        universe = LendingCandleUniverse(lending_candle_type_map)
+
+        # Read all data for a single reserve
+        usdc_variable_borrow = universe.variable_borrow_apr.get_samples_by_pair(usdc_reserve.reserve_id)
+
+        #            reserve_id      open      high       low     close  timestamp
+        # timestamp
+        # 2023-01-01           3  1.836242  1.839224  1.780513  1.780513 2023-01-01
+
+        assert usdc_variable_borrow["open"][pd.Timestamp("2023-01-01")] == pytest.approx(1.836242)
+        assert usdc_variable_borrow["close"][pd.Timestamp("2023-01-01")] == pytest.approx(1.780513)
+
+        # Read data for multiple reserves for a time range
+
+        #             reserve_id      open      high       low     close  timestamp
+        # timestamp
+        # 2023-01-05           6  2.814886  2.929328  2.813202  2.867843 2023-01-05
+        # 2023-01-06           6  2.868013  2.928622  2.829608  2.866523 2023-01-06
+
+        start = pd.Timestamp("2023-01-05")
+        end = pd.Timestamp("2023-01-06")
+        iterator = universe.supply_apr.iterate_samples_by_pair_range(start, end)
+        for reserve_id, supply_apr in iterator:
+            # Read supply apr only for USDT
+            if reserve_id == usdt_reserve.reserve_id:
+                assert len(supply_apr) == 2  # 2 days
+                assert supply_apr["open"][pd.Timestamp("2023-01-05")] == pytest.approx(2.814886)
+                assert supply_apr["close"][pd.Timestamp("2023-01-06")] == pytest.approx(2.866523)
     """
 
     stable_borrow_apr: LendingMetricUniverse | None = None
