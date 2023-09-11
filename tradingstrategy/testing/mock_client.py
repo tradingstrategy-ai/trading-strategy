359501df698e5276f184eeb9fb12d2a4530d8230
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-09-11 11:41:44 +0200

    add common methods to mock client

--- tradingstrategy/testing/mock_client.py
+++ tradingstrategy/testing/mock_client.py
@@ -12,6 +12,8 @@ from pyarrow import Table
 
 from tradingstrategy.client import BaseClient
 from tradingstrategy.timebucket import TimeBucket
+from tradingstrategy.exchange import ExchangeUniverse
+from tradingstrategy.pair import PandasPairUniverse
 
 
 class MockClientNotImplemented(NotImplementedError):
@@ -20,12 +22,6 @@ class MockClientNotImplemented(NotImplementedError):
 
 class MockClient(BaseClient):
     """Have all methods marked as not implemented"""
-
-    def fetch_exchange_universe(self):
-        raise MockClientNotImplemented()
-
-    def fetch_pair_universe(self):
-        raise MockClientNotImplemented()
 
     def fetch_all_candles(self):
         raise MockClientNotImplemented()
@@ -41,6 +37,35 @@ class MockClient(BaseClient):
 
     def fetch_all_liquidity_samples(self, bucket: TimeBucket) -> Table:
         raise MockClientNotImplemented()
+    
+    def get_default_quote_token_address(self, factory_address: str | None = None) -> str:
+        """Get the quote token address used in the generated pair map.
+
+        Helper method for setting up simple local dev routing.
+
+        Returns a the first quote token address found in the pair universe.
+
+        :param factory_address:
+            The factory address to get the quote token address from.
+        """
+        quote_tokens = []
+        pairs_df = self.fetch_pair_universe().to_pandas()
+        pair_universe = PandasPairUniverse(pairs_df)
+        assert pair_universe.get_count() > 0, "Pair universe has no trading pairs"
+        for pair in pair_universe.iterate_pairs():
+            if factory_address:
+                if pair.exchange_address.lower() == factory_address.lower():
+                    quote_tokens.append(pair.quote_token_address)
+            else:
+                quote_tokens.append(pair.quote_token_address)
+        #assert len(quote_tokens) == 1, f"Got {len(quote_tokens)} quote tokens in the pair universe, the pair universe is total {pair_universe.get_count()} pairs"
+        return quote_tokens[0]
+
+    def fetch_exchange_universe(self) -> ExchangeUniverse:
+        return self.exchange_universe
+
+    def fetch_pair_universe(self) -> Table:
+        return self.pairs_table
 
 
 
