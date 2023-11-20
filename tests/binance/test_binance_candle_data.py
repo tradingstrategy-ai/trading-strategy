c976e122a288660c903892ae09bc36040ed34fa6
alex urban <alexurban12@gmail.com>
2023-11-20 13:58:21 +0200

    update tests

--- tests/binance/test_binance_candle_data.py
+++ tests/binance/test_binance_candle_data.py
@@ -1,27 +1,61 @@
 import datetime
+import pytest
+import pandas as pd
+from pathlib import Path
 
-from tradingstrategy.binance.candlestick import fetch_binance_candlestick_data
+from tradingstrategy.binance.candlestick import BinanceCandleDownloader
 from tradingstrategy.timebucket import TimeBucket
 
 
-def test_read_fresh_candle_data():
-    """Test reading fresh candle data."""
-    df = fetch_binance_candlestick_data(
-        "ETHUSDC",
-        TimeBucket.h1,
-        datetime.datetime(2021, 1, 1),
-        datetime.datetime(2021, 1, 2),
+SYMBOL = "ETHUSDC"
+TIME_BUCKET = TimeBucket.h1
+START_AT = datetime.datetime(2021, 1, 1)
+END_AT = datetime.datetime(2021, 1, 2)
+
+
+@pytest.fixture(scope="module")
+def candle_downloader():
+    return BinanceCandleDownloader()
+
+
+@pytest.fixture(scope="module")
+def path(candle_downloader: BinanceCandleDownloader):
+    return candle_downloader.get_parquet_path(SYMBOL, TIME_BUCKET, START_AT, END_AT)
+
+
+def test_read_fresh_candle_data(candle_downloader: BinanceCandleDownloader):
+    """Test reading fresh candle data. Will be downloaded from Binance API."""
+    df = candle_downloader.fetch_candlestick_data(
+        SYMBOL,
+        TIME_BUCKET,
+        START_AT,
+        END_AT,
         force_redownload=True,
     )
 
-    pass
+    assert len(df) == 49
+    assert df.columns.tolist() == ["open", "high", "low", "close", "volume"]
+    assert df.isna().sum().sum() == 0
+    assert df.isna().values.any() == False
 
 
-def test_read_cached_candle_data():
-    """Test reading cached candle data."""
-    pass
+def test_read_cached_candle_data(
+    candle_downloader: BinanceCandleDownloader, path: Path
+):
+    """Test reading cached candle data. Must be run after test_read_fresh_candle_data."""
+    df = candle_downloader.get_data_parquet(SYMBOL, TIME_BUCKET, START_AT, END_AT)
+
+    assert len(df) == 49
+    assert df.columns.tolist() == ["open", "high", "low", "close", "volume"]
+    assert df.isna().sum().sum() == 0
+    assert df.isna().values.any() == False
 
 
-def test_purge_cached_candle_data():
-    """Test purging cached candle data."""
-    pass
+def test_purge_cached_candle_data(candle_downloader: BinanceCandleDownloader, path):
+    """Test purging cached candle data. Must be run after test_read_fresh_candle_data."""
+    assert path.exists() == True
+    candle_downloader.purge_cached_file(path=path)
+    assert path.exists() == False
+
+    candle_downloader.purge_all_cached_data()
+    assert len(list(candle_downloader.cache_directory.iterdir())) == 0
