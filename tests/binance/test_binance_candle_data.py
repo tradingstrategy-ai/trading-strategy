1d7b2694d890a5c86997b41e7ab2b61cb3d1b082
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-11-20 11:15:33 +0200

    add test skeleton

---
+++ tests/binance/test_binance_candle_data.py
@@ -0,0 +1,15 @@
+
+
+
+def test_read_fresh_candle_data():
+    """Test reading fresh candle data."""
+    pass
+
+def test_read_cached_candle_data():
+    """Test reading cached candle data."""
+    pass
+
+def test_purge_cached_candle_data():
+    """Test purging cached candle data."""
+    pass
+
