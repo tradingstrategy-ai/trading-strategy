746c07f348d9e8b9a7eae694501c0ef2a30089fb
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-11-20 11:41:55 +0200

    flesh out first test

--- tests/binance/test_binance_candle_data.py
+++ tests/binance/test_binance_candle_data.py
@@ -1,15 +1,27 @@
+import datetime
 
+from tradingstrategy.binance.candlestick import fetch_binance_candlestick_data
+from tradingstrategy.timebucket import TimeBucket
 
 
 def test_read_fresh_candle_data():
     """Test reading fresh candle data."""
+    df = fetch_binance_candlestick_data(
+        "ETHUSDC",
+        TimeBucket.h1,
+        datetime.datetime(2021, 1, 1),
+        datetime.datetime(2021, 1, 2),
+        force_redownload=True,
+    )
+
     pass
+
 
 def test_read_cached_candle_data():
     """Test reading cached candle data."""
     pass
 
+
 def test_purge_cached_candle_data():
     """Test purging cached candle data."""
     pass
-
