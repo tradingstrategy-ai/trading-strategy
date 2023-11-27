894500ba7d177a1d0cad0961eb40592cd30c253d
Alex <74208897+AlexTheLion123@users.noreply.github.com>
2023-11-27 19:13:59 +0200

    use string_to_eth_address

--- tradingstrategy/binance/constants.py
+++ tradingstrategy/binance/constants.py
@@ -1,9 +1,10 @@
 from tradingstrategy.chain import ChainId
 from tradingstrategy.exchange import ExchangeType
+from tradingstrategy.utils.format import string_to_eth_address
 
 BINANCE_CHAIN_ID = ChainId.unknown
 BINANCE_CHAIN_SLUG = ChainId(BINANCE_CHAIN_ID)
-BINANCE_EXCHANGE_ADDRESS = hex(hash("binance"))
+BINANCE_EXCHANGE_ADDRESS = string_to_eth_address("binance")
 BINANCE_EXCHANGE_SLUG = "binance"
 BINANCE_EXCHANGE_ID = 129875571
 BINANCE_EXCHANGE_TYPE = ExchangeType.uniswap_v2
