50d86a08c6ac9277ef5d3766e5753f0b9b7b4b51
alex urban <alexurban12@gmail.com>
2023-12-04 08:11:22 +0200

    add more utility functions

--- tradingstrategy/binance/utils.py
+++ tradingstrategy/binance/utils.py
@@ -1,4 +1,7 @@
-"""Placeholder for Binance data."""
+"""Utility functions to help automatically generate Binance data."""
+import pandas as pd
+from eth_typing import HexAddress
+
 
 from tradingstrategy.pair import DEXPair
 from tradingstrategy.binance.constants import (
@@ -15,6 +18,13 @@ from tradingstrategy.binance.constants import (
 from tradingstrategy.utils.format import string_to_eth_address
 from tradingstrategy.exchange import Exchange, ExchangeUniverse
 from tradingstrategy.chain import ChainId
+from tradingstrategy.utils.format import string_to_eth_address
+from tradingstrategy.lending import (
+    LendingReserve,
+    LendingProtocolType,
+    LendingReserveAdditionalDetails,
+)
+from tradingstrategy.pair import DEXPair
 
 
 def generate_pairs_for_binance(
@@ -90,3 +100,84 @@ def generate_exchange_for_binance(pair_count: int) -> Exchange:
 def generate_exchange_universe_for_binance(pair_count: int) -> ExchangeUniverse:
     """Generate an exchange universe for Binance data."""
     return ExchangeUniverse.from_collection([generate_exchange_for_binance(pair_count)])
+
+
+def add_info_columns_to_ohlc(df: pd.DataFrame, pairs: dict[str, DEXPair]):
+    """Add single pair informational columns to an OHLC dataframe.
+
+    :param *args: Each argument is a dict with the format {symbol: pair}
+        E.g. {'ETHUSDT': TradingPairIdentifier(...)}
+
+    :return: The same dataframe with added columns
+    """
+
+    for symbol, pair in pairs.items():
+        if symbol not in df["pair_id"].unique():
+            raise ValueError(f"Symbol {symbol} not found in DataFrame")
+
+        # Update the DataFrame only for the rows where 'symbol' matches
+        mask = df["pair_id"] == symbol
+        df.loc[mask, "base_token_symbol"] = pair.base_token_symbol
+        df.loc[mask, "quote_token_symbol"] = pair.quote_token_symbol
+        df.loc[mask, "exchange_slug"] = pair.exchange_slug
+        df.loc[mask, "chain_id"] = pair.chain_id
+        df.loc[mask, "fee"] = pair.fee / 10_000
+        df.loc[mask, "buy_volume_all_time"] = 0
+        df.loc[mask, "address"] = pair.address
+        df.loc[mask, "exchange_id"] = BINANCE_EXCHANGE_ID
+        df.loc[mask, "token0_address"] = pair.base_token_address
+        df.loc[mask, "token1_address"] = pair.quote_token_address
+        df.loc[mask, "token0_symbol"] = pair.base_token_symbol
+        df.loc[mask, "token1_symbol"] = pair.quote_token_symbol
+        df.loc[mask, "token0_decimals"] = pair.base_token_decimals
+        df.loc[mask, "token1_decimals"] = pair.quote_token_decimals
+
+    return df
+
+
+def generate_lending_reserve_for_binance(
+    asset_symbol: str,
+    address: HexAddress,
+    reserve_id: int,
+    asset_decimals=18,
+) -> LendingReserve:
+    """Generate a lending reserve for Binance data.
+
+    Binance data is not on-chain, so we need to generate the identifiers
+    for the trading pairs.
+
+    :param asset_symbol: E.g. `ETH`
+    :param address: address of the reserve
+    :param reserve_id: id of the reserve
+    :return: LendingReserve
+    """
+
+    assert isinstance(reserve_id, int), f"Bad reserve_id {reserve_id}"
+
+    atoken_symbol = f"a{asset_symbol.upper()}"
+    vtoken_symbol = f"v{asset_symbol.upper()}"
+
+    return LendingReserve(
+        reserve_id=reserve_id,
+        reserve_slug=asset_symbol.lower(),
+        protocol_slug=LendingProtocolType.aave_v3,
+        chain_id=BINANCE_CHAIN_ID,
+        chain_slug=BINANCE_CHAIN_SLUG,
+        asset_id=1,
+        asset_symbol=asset_symbol,
+        asset_address=address,
+        asset_decimals=asset_decimals,
+        atoken_id=1,
+        asset_name=asset_symbol,
+        atoken_symbol=atoken_symbol,
+        atoken_address=string_to_eth_address(atoken_symbol),
+        atoken_decimals=18,
+        vtoken_id=1,
+        vtoken_symbol=vtoken_symbol,
+        vtoken_address=string_to_eth_address(vtoken_symbol),
+        vtoken_decimals=18,
+        additional_details=LendingReserveAdditionalDetails(
+            ltv=0.825,
+            liquidation_threshold=0.85,
+        ),
+    )
