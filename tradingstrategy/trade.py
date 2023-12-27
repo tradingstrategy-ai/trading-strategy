1215798788ee755cf5ad4446ceddb20adecd2858
Mikko Ohtamaa <mikko@opensourcehacker.com>
2023-12-27 13:50:03 +0200

    Started to work trade export format

---
+++ tradingstrategy/trade.py
@@ -0,0 +1,31 @@
+"""Individual DEX trade data."""
+from dataclasses import dataclass
+
+
+@dataclass
+class Trade:
+    """Individual trade.
+
+    Describe the data structure used for individual trades in DEX trades dataset.
+
+    - Dataset contains all supported DEX trade across supported blockchains
+
+    - Based on events
+
+    - Data can be cross-referenced to blockchain transactions and log indexed
+
+    - Trades are Swaps for Uniswap
+
+    - Dataset focuses on executed price, for market price feed
+      see candle datasets
+
+    - Trading pairs are normalised to base token - quote token
+      human-readable format, as opposite to raw Uniswap token pairs
+
+    - The approximate best effort USD exchange rate is included,
+      when available, to calculate US dollar measured impact of trades
+
+    - Dataset is sorted for the best file compression - trades
+      are not guaranteed to be ordered by time
+    """
+
