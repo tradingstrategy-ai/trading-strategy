0956950334480b39a724e67e42d25fb613163b89
Mikko Ohtamaa <mikko@opensourcehacker.com>
2024-01-18 09:51:34 +0100

    Adding metadata

--- tradingstrategy/trade.py
+++ tradingstrategy/trade.py
@@ -1,5 +1,11 @@
 """Individual DEX trade data."""
+import datetime
 from dataclasses import dataclass
+
+import pyarrow as pa
+
+from tradingstrategy.chain import ChainId
+from tradingstrategy.types import PrimaryKey, BlockNumber
 
 
 @dataclass
@@ -28,4 +34,90 @@ class Trade:
     - Dataset is sorted for the best file compression - trades
       are not guaranteed to be ordered by time
     """
+
+    #: The blockchain where this swap happened
+    chain_id: ChainId
+
+    #: The trading pair internal id
+    #:
+    #: See :py:class:`tradingstrategy.pair.PandasPairUniverse` how to resolve
+    #: internal ids to pair metadata.
+    #:
+    pair_id: PrimaryKey
+
+    #: The block where this event happened
+    #:
+    block_number: BlockNumber
+
+    #: The block production timestamp
+    #:
+    timestamp: datetime.datetime
+
+    #: Transaction hash
+    #:
+    #: 32 bytes.
+    #:
+    tx_hash: bytes
+
+    #: Swap event index within a block
+    #:
+    log_index: int
+
+    #: How much quote token was traded in this trade.
+    #:
+    #: Quote token is identified from the :py:attr:`pair_id`
+    #: metadata.
+    #:
+    quote_token_diff: float
+
+    #: How much base token was traded in this trade.
+    #:
+    #: Base token is identified from the :py:attr:`pair_id`
+    #: metadata.
+    #:
+    base_token_diff: float
+
+    #: Quote token/USD exchange rate (if available).
+    #:
+    #: The approximate USD exchange rate to convert the trade
+    #: value to dollars. Only available for some
+    #: quoet tokens. It's an approximatino of a close
+    #: time, so only suitable for statistical analysis.
+    #:
+    #: Set to zero if not available.
+    #:
+    usd_exchange_rate: float
+
+    #: The trading pair/pool contract address.
+    #:
+    #: 160 bits, or 20 bytes.
+    #:
+    pool_address: bytes
+
+    #: The transsaction originator address.
+    sender_address: bytes
+
+    #: The DEX internal id.
+    #:
+    #: Can be resolved with :py:class:`tradingstrategy.exchange.ExchangeUniverse`
+    #:
+    exchange_id: int
+
+    @staticmethod
+    def to_pyarrow_schema() -> pa.Schema:
+        return pa.schema([
+            ("chain_id", pa.uint32()),
+            ("pair_id", pa.uint64()),
+            ("block_number", pa.uint32()),
+            ("timestamp", pa.timestamp("s")),
+            ("tx_hash", pa.binary(32)),
+            ("log_index", pa.uint32()),
+            ("quote_token_diff", pa.float64()),
+            ("base_token_diff", pa.float64()),
+            ("usd_exchange_rate", pa.float64()),
+            ("pool_address", pa.binary(20)),
+            ("sender_address", pa.binary(20)),
+            ("exchange_id", pa.uint32()),
+        ])
+
 
