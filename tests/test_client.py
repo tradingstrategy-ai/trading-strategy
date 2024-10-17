1512a9915887358c3c9c7e9a36dce5821c6624b7
Mikko Ohtamaa <mikko@redinnovation.com>
2024-10-17 19:24:50 +0200

    Add module imports to Pyodide test to cover we can install without optional deps

--- tests/test_client.py
+++ tests/test_client.py
@@ -137,6 +137,19 @@ def test_create_pyodide_client_detect():
     env = JupyterEnvironment()
     env.clear_configuration()
     client = Client.create_jupyter_client(pyodide=True)
+
+    # See we can import important modules
+    # Before we had:
+    # ModuleNotFound: eth_defi when importing lending
+    import tradingstrategy.utils.token_filter
+    import tradingstrategy.lending
+    import tradingstrategy.stablecoin
+    import tradingstrategy.utils.wrangle
+    import tradingstrategy.pair
+    import tradingstrategy.candle
+    import tradingstrategy.liquidity
+    import tradingstrategy.clmm
+    import tradingstrategy.exchange
     assert isinstance(client, Client)
 
 
