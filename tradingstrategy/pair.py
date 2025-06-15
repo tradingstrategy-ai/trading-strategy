9fe3e779a7a3c0bc58109df9f367cab1db9e5e4b
Mikko Ohtamaa <mikko@redinnovation.com>
2025-06-15 23:38:23 +0300

    Better NaN handling in token metadata

--- tradingstrategy/pair.py
+++ tradingstrategy/pair.py
@@ -2179,16 +2179,15 @@ def _convert_to_dex_pair(data: dict, exchange_universe: ExchangeUniverse | None=
         meta = data.get("token_metadata")
 
         # Received NaN as token metadata column, should probably be None?"
-        if meta == np.nan:
+        if pd.isna(meta):
             meta = None
+
+        if obj.other_data is None:
+            obj.other_data = {}
 
         match meta:
             case TokenMetadata():
                 assert isinstance(meta, TokenMetadata), f"Expected TokenMetadata, got {type(meta)}: {meta}"
-
-                if obj.other_data is None:
-                    obj.other_data = {}
-
                 obj.other_data["token_metadata"] = meta
                 obj.buy_tax =  meta.get_buy_tax()
                 obj.sell_tax = meta.get_sell_tax()
