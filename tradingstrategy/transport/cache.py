ec54255ee4f3ea0fa2f02093bb692f7125fe42fb
Ken Kunz <kennethkunz@gmail.com>
2025-10-02 16:04:45 -0500

    Extract wait_other_writers to cache_utils.py

--- tradingstrategy/transport/cache.py
+++ tradingstrategy/transport/cache.py
@@ -9,7 +9,6 @@ import pathlib
 import platform
 import re
 import time
-from contextlib import contextmanager
 from http.client import IncompleteRead
 from importlib.metadata import version, PackageNotFoundError
 from json import JSONDecodeError
@@ -36,6 +35,7 @@ from tradingstrategy.lending import LendingCandle, LendingCandleType
 from tradingstrategy.liquidity import XYLiquidity
 from tradingstrategy.timebucket import TimeBucket
 from tradingstrategy.token_metadata import TokenMetadata
+from tradingstrategy.transport.cache_utils import wait_other_writers
 from tradingstrategy.transport.jsonl import load_candles_jsonl, load_token_metadata_jsonl
 from tradingstrategy.transport.pair_candle_cache import PairCandleMetadata
 from tradingstrategy.transport.progress_enabled_download import download_with_tqdm_progress_bar
@@ -1386,77 +1386,6 @@ class CachedHTTPTransport:
 
         # Return and convert to TokenMetadata instances
         return {address: TokenMetadata(**item) for address, item in full_set.items()}
-
-
-@contextmanager
-def wait_other_writers(path: Path | str, timeout=120):
-    """Wait other potential writers writing the same file.
-
-    - Work around issues when parallel unit tests and such
-      try to write the same file
-
-    Example:
-
-    .. code-block:: python
-
-        import urllib
-        import tempfile
-
-        import pytest
-        import pandas as pd
-
-        @pytest.fixture()
-        def my_cached_test_data_frame() -> pd.DataFrame:
-
-            # Al tests use a cached dataset stored in the /tmp directory
-            path = os.path.join(tempfile.gettempdir(), "my_shared_data.parquet")
-
-            with wait_other_writers(path):
-
-                # Read result from the previous writer
-                if not path.exists(path):
-                    # Download and write to cache
-                    urllib.request.urlretrieve("https://example.com", path)
-
-                return pd.read_parquet(path)
-
-    :param path:
-        File that is being written
-
-    :param timeout:
-        How many seconds wait to acquire the lock file.
-
-        Default 2 minutes.
-
-    :raise filelock.Timeout:
-        If the file writer is stuck with the lock.
-    """
-
-    if type(path) == str:
-        path = Path(path)
-
-    assert isinstance(path, Path), f"Not Path object: {path}"
-
-    assert path.is_absolute(), f"Did not get an absolute path: {path}\n" \
-                               f"Please use absolute paths for lock files to prevent polluting the local working directory."
-
-    # If we are writing to a new temp folder, create any parent paths
-    os.makedirs(path.parent, exist_ok=True)
-
-    # https://stackoverflow.com/a/60281933/315168
-    lock_file = path.parent / (path.name + '.lock')
-
-    lock = FileLock(lock_file, timeout=timeout)
-
-    if lock.is_locked:
-        logger.info(
-            "Parquet file %s locked for writing, waiting %f seconds",
-            path,
-            timeout,
-        )
-
-    with lock:
-        yield
 
 
 def _check_good_json(path: Path, exception_message: str):
